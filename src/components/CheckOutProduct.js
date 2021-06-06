3f220e9d2e329aeeed66b67e80741160ffd7f521
abhiraj <thakurabhiraj4@gmail.com>
2021-06-06 19:03:28 +0530

    basket functionality

---
+++ src/components/CheckOutProduct.js
@@ -0,0 +1,44 @@
+import { StarIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import Currency from "react-currency-formatter";
+
+function CheckOutProduct({
+  id,
+  title,
+  price,
+  rating,
+  description,
+  category,
+  image,
+  hasPrime,
+}) {
+  return (
+    <div className="grid grid-cols-5">
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <div className="col-span-3 mx-5">
+        <p>{title}</p>
+        <div className="flex">
+          {Array(rating).fill().map((_, i) => (
+            <StarIcon key={i} className="h-5 text-yellow-500" />
+          ))}
+        </div>
+
+        <p className='text-xs my-2 line-clamp-3'>{description}</p>
+        <Currency quantity={price*100} currency="INR" />
+        {hasPrime && (
+          <div className='flex items-center space-x-2'>
+            <img 
+              loading="lazy" 
+              className='w-12'
+              src="https://links.papareact.com/fdw" 
+              alt="" 
+            />
+            <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default CheckOutProduct;
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,55 @@
+import Image from "next/image";
+import { useSelector } from "react-redux";
+import CheckOutProduct from "../components/CheckOutProduct";
 import Header from "../components/Header";
+import { selectItems } from "../slices/basketSlice";
 
 function Checkout() {
+  const items= useSelector(selectItems);
+
   return (
-    <div className='bg-gray-100'>
+    <div className="bg-gray-100">
       <Header />
+
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* left */}
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {
+                items.length === 0
+                ? "Your Amazon Basket is empty."
+                : "Shopping Basket"
+              }
+            </h1>
+
+            {items.map((item, i) => (
+              <CheckOutProduct 
+                key={i}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                description={item.description} 
+                category={item.category} 
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
+
+          </div>
+        </div>
+
+        {/* right */}
+      </main>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
