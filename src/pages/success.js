2a2fbc0032656cab9de05f7bbc1111ea297b8454
abhiraj <thakurabhiraj4@gmail.com>
2021-06-13 12:07:57 +0530

    success page fix

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,7 +6,7 @@ function success() {
   return (
     <div className="bg-gray-100 h-screen">
       <Header />
-      <main className="mx-w-screen-lg mx-auto">
+      <main className="max-w-screen-lg mx-auto">
         <div className="flex flex-col p-10 bg-white">
           <div className="flex items-center space-x-2 mb-5">
             <CheckCircleIcon className="text-green-500 h-10" />
